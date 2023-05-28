3d5c089987122100d5dbc8eb09fd02d7cd11f44c
Sarvesh <sarveshbhushan575@gmail.com>
2023-05-28 22:22:05 +0530

    Final commit

---
+++ api/routes/auth.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { login, register } from "../contollers/auth.js";
+const router=express.Router();
+
+router.post("/register", register)
+router.post("/login", login)
+export default router;
---
+++ api/routes/hotels.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel, countByCity, countByType, getHotelRooms } from "../contollers/hotel.js";
+
+import Hotel from "../models/Hotel.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+//import {verifyAdmin} from "../utils/verifyToken.js"
+const router = express.Router();
+
+//CREATE
+router.post("/",verifyAdmin,  createHotel);
+
+//UPDATE
+router.put("/:id",verifyAdmin,updateHotel);
+//DELETE
+router.delete("/:id",verifyAdmin, deleteHotel);
+//GET
+
+router.get("/find/:id", getHotel);
+//GET ALL
+
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms)
+export default router;
